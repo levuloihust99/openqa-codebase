c948d808b52f16198dcfdd40d855e2cef49fd953
levuloihust99 <levuloihust@outlook.com>
2021-05-30 06:53:50 +0000

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+cat >> ~/.bashrc << .
+git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \$(git_branch)\$ "
+.
+
+sudo apt update
+sudo apt -y install python3-venv python3-dev build-essential
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip install -U pip
+pip install -r requirements.txt
