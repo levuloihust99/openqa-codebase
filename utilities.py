2c14c15fcd9b537d603aa2fc05ae04c23786203e
levuloihust99 <loi.lv173240@sis.hust.edu.vn>
2021-06-14 14:50:51 +0700

    tf-torch compability check

--- utilities.py
+++ utilities.py
@@ -2,6 +2,7 @@ from argparse import Namespace
 import yaml
 import os
 from typing import Dict
+from transformers import BertModel, TFBertModel
 
 
 def write_config(config_path: str, config: Dict):
@@ -59,3 +60,24 @@ def spread_samples_equally(global_batch_size, num_replicas, base_replica_batch_s
 
     remainder = global_batch_size - base_replica_batch_size * num_replicas
     return [base_replica_batch_size] * num_replicas, remainder, base_replica_batch_size
+
+
+def check_compability(
+    torch_model: BertModel,
+    tf_model: TFBertModel
+):
+    torch_weights = []
+    for k, v in torch_model.state_dict().items():
+        if k == "embeddings.position_ids":
+            print("im here")
+            continue
+        if not k.startswith("embeddings.") and k.endswith(".weight"):
+            torch_weights.append(v.t().numpy())
+        else:
+            torch_weights.append(v.numpy())
+    torch_weights[1], torch_weights[2] = torch_weights[2], torch_weights[1]
+
+    tf_weights = tf_model.get_weights()
+
+    check = [(torch_weight == tf_weight).all() for torch_weight, tf_weight in zip(torch_weights, tf_weights)]
+    return all(check)
